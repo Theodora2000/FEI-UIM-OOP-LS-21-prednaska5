90e3026c79243f7d744189c72fe56bf32536ded8
Theodora2000 <xsimont@stuba.sk>
2021-05-12 13:17:46 +0200

    prednaska6, pr2 layouts

--- src/sk/stuba/uim/fei/oop/Test.java
+++ src/sk/stuba/uim/fei/oop/Test.java
@@ -7,13 +7,14 @@ public class Test {
     public static void main(String args[]){
 
         JFrame F = new JFrame();
-        F.setSize(300,200);
-        F.setLayout(new BorderLayout());
-        F.add(BorderLayout.CENTER,new TestCanvas());
-        JPanel p = new JPanel();
-        p.add(new JButton("Change"));
-        p.add(new JButton("Close"));
-        F.add(BorderLayout.SOUTH,p);
+
+        F.setLayout(new GridLayout(0,2));
+        F.add(new JLabel("riadok1, stlpec1"));
+        F.add(new JLabel("riadok1, stlpec2"));
+        F.add(new JLabel("riadok2, stlpec1"));
+        F.add(new JLabel("riadok2, stlpec2"));
+        F.add(new JLabel("riadok3, stlpec1")) ;
+        F.pack();//spakuje to do najlepseho menseho tvaru
         F.setVisible(true);
 
     }
--- src/sk/stuba/uim/fei/oop/TestCanvas.java
+++ src/sk/stuba/uim/fei/oop/TestCanvas.java
@@ -6,23 +6,6 @@ public class TestCanvas extends Canvas {
 
     public void paint(Graphics g){
 
-        Dimension  d =  getSize();
-        int w = d.width;
-        int h = d.height;
 
-        int i=1;
-        Color C;
-
-        while(i<((w-1)-i) && i<((h-1)-i)){
-            if(2*i < 256){
-                C=new Color(2*i, 255-2*i, 255);
-            }
-            else{
-                C = new Color(255,0,255);
-            }
-            g.setColor(C);
-            g.drawRect(i,i,(w-1)-(2*i),(h-1)-(2*i));
-            i++;
-        }
     }
 }
