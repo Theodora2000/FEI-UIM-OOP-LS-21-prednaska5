5e71d2e2a2a54004d8e170742b8dd59597d5a190
Theodora2000 <xsimont@stuba.sk>
2021-05-12 13:04:20 +0200

    prednaska6, pr1 addbuttons

--- src/sk/stuba/uim/fei/oop/Test.java
+++ src/sk/stuba/uim/fei/oop/Test.java
@@ -5,9 +5,15 @@ import java.awt.*;
 
 public class Test {
     public static void main(String args[]){
+
         JFrame F = new JFrame();
         F.setSize(300,200);
-        F.add(new TestCanvas());
+        F.setLayout(new BorderLayout());
+        F.add(BorderLayout.CENTER,new TestCanvas());
+        JPanel p = new JPanel();
+        p.add(new JButton("Change"));
+        p.add(new JButton("Close"));
+        F.add(BorderLayout.SOUTH,p);
         F.setVisible(true);
 
     }
