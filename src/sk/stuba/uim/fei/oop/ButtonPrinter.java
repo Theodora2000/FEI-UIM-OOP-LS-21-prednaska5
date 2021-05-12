6081d15709beb978411b2afd4b81b2795f484984
Theodora2000 <xsimont@stuba.sk>
2021-05-12 13:47:31 +0200

    prednaska6, pr3 button event

---
+++ src/sk/stuba/uim/fei/oop/ButtonPrinter.java
@@ -0,0 +1,18 @@
+package sk.stuba.uim.fei.oop;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonPrinter implements ActionListener {
+
+    int pocet = 0;
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        pocet++;
+        // getActionCommand je vlastne nazov toho buttonu
+        System.out.println(e.getActionCommand() + " " + pocet);
+    }
+}
--- src/sk/stuba/uim/fei/oop/Test.java
+++ src/sk/stuba/uim/fei/oop/Test.java
@@ -7,14 +7,12 @@ public class Test {
     public static void main(String args[]){
 
         JFrame F = new JFrame();
-
-        F.setLayout(new GridLayout(0,2));
-        F.add(new JLabel("riadok1, stlpec1"));
-        F.add(new JLabel("riadok1, stlpec2"));
-        F.add(new JLabel("riadok2, stlpec1"));
-        F.add(new JLabel("riadok2, stlpec2"));
-        F.add(new JLabel("riadok3, stlpec1")) ;
-        F.pack();//spakuje to do najlepseho menseho tvaru
+        F.setSize(300,300);
+        JPanel p = new JPanel();
+        JButton b = new JButton("OK");
+        b.addActionListener(new ButtonPrinter());
+        p.add(b);
+        F.add(p);
         F.setVisible(true);
 
     }
